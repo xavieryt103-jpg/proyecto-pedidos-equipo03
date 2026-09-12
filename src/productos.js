8767ffdeb70ff8e7df6929ece71b5c5ea0ebeb58
Anthony Mejia <mdanthony04@gmail.com>
2026-09-12 15:12:10 -0500

    feat: implementar listado de productos

---
+++ src/productos.js
@@ -0,0 +1,10 @@
+// Módulo de Productos - Don Rosel (Café y Cacao)
+// Integrante: Anthony Mejia Diaz
+
+function registrarProducto(nombre, tipo, precio) {
+    console.log("Producto registrado exitosamente: " + nombre);
+}
+
+function listarProductos() {
+    console.log("Mostrando catálogo de productos Don Rosel...");
+}
